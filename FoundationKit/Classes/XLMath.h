0a25e14c2aa6f108d53697a0527af77fa92ef121
hongketao <coderhong@126.com>
2021-04-12 18:02:59 +0800

    添加XLMath

---
+++ FoundationKit/Classes/XLMath.h
@@ -0,0 +1,18 @@
+//
+//  XLMath.h
+//  FoundationKit
+//
+//  Created by coderhong on 2021/4/12.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XLMath : NSObject
+
++ (int)sum:(int)num1 num2:(int)num2;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FoundationKit/Classes/XLMath.m
@@ -0,0 +1,17 @@
+//
+//  XLMath.m
+//  FoundationKit
+//
+//  Created by coderhong on 2021/4/12.
+//
+
+#import "XLMath.h"
+
+@implementation XLMath
+
++ (int)sum:(int)num1 num2:(int)num2
+{
+    return num1 + num2;
+}
+
+@end
